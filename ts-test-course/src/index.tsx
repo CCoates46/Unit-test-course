f3c582c8005b3857c8f768828190e872e9f6a78b
CCoates46 <clare_coates37@yahoo.com>
2023-06-30 16:09:25 +0100

    added react tests

---
+++ ts-test-course/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App2 from './App2';
+import LoginComponent from './LoginComponent';
+import LoginService from './services/LoginService';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const loginService = new LoginService()
+
+const setToken = (token: string) => {
+  console.log(`received the token ${token}`)
+}
+
+root.render(
+    <LoginComponent loginService={loginService} setToken={setToken}/>
+);
